71d8db06c133501930a1b1673e2e74c6ac13b9ed
Varsha8738 <sreevarshapenta@gmail.com>
2024-10-17 10:37:11 +0530

    Create Second.py

---
+++ Second.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate
+# string slicing
+# String slicing
+String = 'SASICSE'
+# Using indexing sequence
+print(String[1:5:2])
+# Using indexing sequence
+print(String[-1:-12:-2])
+# Prints string in reverse
+print(String[::-1])
+-->OUTPUT
+AI
+ECSS
+ESCISAS
