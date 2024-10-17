f51c514c9d1dda170d1be5406cee78b41059ec62
Varsha8738 <sreevarshapenta@gmail.com>
2024-10-17 10:40:37 +0530

    Create Fifth.py

---
+++ Fifth.py
@@ -0,0 +1,11 @@
+list1= ["apple", 11,23,4.5,"banana"]
+print(list1)
+del list1[0]
+print(list1)
+list2=[1,2,3,4,5,6,7,8,9,10]
+del list2[1:4]
+print(list2)
+-->OUTPUT
+['apple', 11, 23, 4.5, 'banana']
+[11, 23, 4.5, 'banana']
+[1, 5, 6, 7, 8, 9, 10]
